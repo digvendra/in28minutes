5754d47b3178c7164bcacc4b7edb5d0c9d84c464
digvendra <digvendra@hotmail.com>
2020-01-05 00:59:19 +0530

    step 10 completed

--- springboot/firstwebapp/spring-boot-advance-web-application/src/main/java/com/springboot/advance/controller/SurveyController.java
+++ springboot/firstwebapp/spring-boot-advance-web-application/src/main/java/com/springboot/advance/controller/SurveyController.java
@@ -1,11 +1,16 @@
 package com.springboot.advance.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.springboot.advance.model.Question;
 import com.springboot.advance.service.SurveyService;
@@ -19,6 +24,22 @@ public class SurveyController {
 	@GetMapping("surveys/{surveyId}/questions")
 	public List<Question> retrieveQuestionsForSurvey(@PathVariable String surveyId) {
 		return surveyService.retrieveQuestions(surveyId);
+	}
+	
+	@GetMapping("surveys/{surveyId}/questions/{questionId}")
+	public Question retrieveQuestion(@PathVariable String surveyId,@PathVariable String questionId) {
+		return surveyService.retrieveQuestion(surveyId, questionId);
+	}
+	
+	@PostMapping("surveys/{surveyId}/questions")
+	public ResponseEntity<?> addQuestionToSurvey(@PathVariable String surveyId, @RequestBody Question newQuestion) {
+		Question question = surveyService.addQuestion(surveyId, newQuestion);
+		if(null == question) {
+			return ResponseEntity.noContent().build();
+		}
+		
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{questionId}").buildAndExpand(question.getId()).toUri();
+		return ResponseEntity.created(location).build();
 	}
 
 }
