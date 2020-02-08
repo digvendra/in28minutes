ded6afa29c8af3a39df3f629c3afe68a5f72842f
digvendra <digvendra@hotmail.com>
2020-02-09 00:39:44 +0530

    first rest structure

---
+++ springboot/firstwebapp/spring-boot-restful-app/src/main/java/com/in28minutes/springboot/restapp/springbootrestfulapp/SpringBootRestfulAppApplication.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.restapp.springbootrestfulapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootRestfulAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootRestfulAppApplication.class, args);
+	}
+
+}
---
+++ springboot/firstwebapp/spring-boot-restful-app/src/test/java/com/in28minutes/springboot/restapp/springbootrestfulapp/SpringBootRestfulAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.springboot.restapp.springbootrestfulapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootRestfulAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
