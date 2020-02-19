b0e2b6e07c15d0c5bc6f8edc95ebd56a95df0424
digvendra <digvendra@hotmail.com>
2020-02-20 01:54:45 +0530

    till step 10. Enhancing POST Method to return correct HTTP Status Code and Location URI

--- springboot/firstwebapp/spring-boot-restful-app/src/main/java/com/in28minutes/springboot/restapp/controller/UserController.java
+++ springboot/firstwebapp/spring-boot-restful-app/src/main/java/com/in28minutes/springboot/restapp/controller/UserController.java
@@ -1,13 +1,16 @@
 package com.in28minutes.springboot.restapp.controller;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.in28minutes.springboot.restapp.bean.User;
 import com.in28minutes.springboot.restapp.dao.UserDaoService;
@@ -29,7 +32,9 @@ public class UserController {
 	}
 	
 	@PostMapping(path = "/users")
-	public void createUser(@RequestBody User user) {
-		userService.save(user);
+	public ResponseEntity createUser(@RequestBody User user) {
+		User newUser = userService.save(user);
+		URI location = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(newUser.getId()).toUri();
+		return ResponseEntity.created(location).build();
 	}
 }
