8a1059832d27c21ff5cd5176224ce354c49f93c6
digvendra <digvendra@hotmail.com>
2020-02-19 08:02:30 +0530

    hello world bean

---
+++ springboot/firstwebapp/spring-boot-restful-app/src/main/java/com/in28minutes/springboot/restapp/bean/HelloWorldBean.java
@@ -0,0 +1,25 @@
+package com.in28minutes.springboot.restapp.bean;
+
+public class HelloWorldBean {
+	
+	String message;
+
+	public HelloWorldBean(String message) {
+		super();
+		this.message = message;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	@Override
+	public String toString() {
+		return "HelloWorldBean [message=" + message + "]";
+	}
+	
+}
--- springboot/firstwebapp/spring-boot-restful-app/src/main/java/com/in28minutes/springboot/restapp/controller/HelloWorldController.java
+++ springboot/firstwebapp/spring-boot-restful-app/src/main/java/com/in28minutes/springboot/restapp/controller/HelloWorldController.java
@@ -3,12 +3,19 @@ package com.in28minutes.springboot.restapp.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.in28minutes.springboot.restapp.bean.HelloWorldBean;
+
 @RestController
 public class HelloWorldController {
 	
 	@GetMapping(path="/hello-world")
 	public String helloWorld() {
 		return "Hello World";
+	}
+	
+	@GetMapping(path="/hello-world-bean")
+	public HelloWorldBean helloWorldBean() {
+		return new HelloWorldBean("Hi Pushkal");
 	}
 
 }
