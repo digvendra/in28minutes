c95ac561b08721db7a205931398baeb7e7170e0a
digvendra <digvendra@hotmail.com>
2020-02-25 07:18:08 +0530

    step 18, internationalization i18n basic

--- springboot/firstwebapp/spring-boot-restful-app/src/main/java/com/in28minutes/springboot/restapp/controller/HelloWorldController.java
+++ springboot/firstwebapp/spring-boot-restful-app/src/main/java/com/in28minutes/springboot/restapp/controller/HelloWorldController.java
@@ -1,13 +1,27 @@
 package com.in28minutes.springboot.restapp.controller;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.in28minutes.springboot.restapp.bean.HelloWorldBean;
 
 @RestController
 public class HelloWorldController {
+	
+	@Autowired
+	MessageSource messageSource;
+	
+	
+	@GetMapping(path="/hello-world-i18n")
+	public String helloWorldi18n(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+		return messageSource.getMessage("good.morning.message", null, locale);
+	}
 	
 	@GetMapping(path="/hello-world")
 	public String helloWorld() {
--- springboot/firstwebapp/spring-boot-restful-app/src/main/java/com/in28minutes/springboot/restapp/springbootrestfulapp/SpringBootRestfulAppApplication.java
+++ springboot/firstwebapp/spring-boot-restful-app/src/main/java/com/in28minutes/springboot/restapp/springbootrestfulapp/SpringBootRestfulAppApplication.java
@@ -1,8 +1,14 @@
 package com.in28minutes.springboot.restapp.springbootrestfulapp;
 
+import java.util.Locale;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.support.ResourceBundleMessageSource;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 @SpringBootApplication
 @ComponentScan("com.in28minutes.springboot.restapp")
@@ -10,6 +16,20 @@ public class SpringBootRestfulAppApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootRestfulAppApplication.class, args);
+	}
+	
+	@Bean
+	public LocaleResolver localeResolver() {
+		SessionLocaleResolver local = new SessionLocaleResolver();
+		local.setDefaultLocale(Locale.US);
+		return local;
+	}
+	
+	@Bean
+	public ResourceBundleMessageSource messageSource() {
+		ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+		source.setBasename("message");
+		return source;
 	}
 
 }
