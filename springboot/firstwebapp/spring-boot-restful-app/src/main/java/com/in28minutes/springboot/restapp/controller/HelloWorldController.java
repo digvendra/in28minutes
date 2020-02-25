f34a8f63a50c4b3c48f266d052d8b366c98d1fed
digvendra <digvendra@hotmail.com>
2020-02-25 07:53:17 +0530

    step 18 - Part 2, Simplification of internationalization i18n basic

--- springboot/firstwebapp/spring-boot-restful-app/src/main/java/com/in28minutes/springboot/restapp/controller/HelloWorldController.java
+++ springboot/firstwebapp/spring-boot-restful-app/src/main/java/com/in28minutes/springboot/restapp/controller/HelloWorldController.java
@@ -1,12 +1,10 @@
 package com.in28minutes.springboot.restapp.controller;
 
-import java.util.Locale;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.in28minutes.springboot.restapp.bean.HelloWorldBean;
@@ -18,9 +16,18 @@ public class HelloWorldController {
 	MessageSource messageSource;
 	
 	
+	/*
+	 * @GetMapping(path="/hello-world-i18n") public String
+	 * helloWorldi18n(@RequestHeader(name = "Accept-Language", required = false)
+	 * Locale locale) { return messageSource.getMessage("good.morning.message",
+	 * null, locale); }
+	 * 
+	 * Don't use Accept Language in request param, instead use LocaleContextHolder
+	 */
+	
 	@GetMapping(path="/hello-world-i18n")
-	public String helloWorldi18n(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
-		return messageSource.getMessage("good.morning.message", null, locale);
+	public String helloWorldi18n() {
+		return messageSource.getMessage("good.morning.message", null, LocaleContextHolder.getLocale());
 	}
 	
 	@GetMapping(path="/hello-world")
--- springboot/firstwebapp/spring-boot-restful-app/src/main/java/com/in28minutes/springboot/restapp/springbootrestfulapp/SpringBootRestfulAppApplication.java
+++ springboot/firstwebapp/spring-boot-restful-app/src/main/java/com/in28minutes/springboot/restapp/springbootrestfulapp/SpringBootRestfulAppApplication.java
@@ -8,7 +8,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import net.bytebuddy.asm.Advice.Local;
 
 @SpringBootApplication
 @ComponentScan("com.in28minutes.springboot.restapp")
@@ -18,18 +21,35 @@ public class SpringBootRestfulAppApplication {
 		SpringApplication.run(SpringBootRestfulAppApplication.class, args);
 	}
 	
-	@Bean
-	public LocaleResolver localeResolver() {
-		SessionLocaleResolver local = new SessionLocaleResolver();
-		local.setDefaultLocale(Locale.US);
-		return local;
-	}
+	/*
+	 * @Bean public LocaleResolver localeResolver() { SessionLocaleResolver local =
+	 * new SessionLocaleResolver(); local.setDefaultLocale(Locale.US); return local;
+	 * }
+	 * 
+	 * new approach is below to use AcceptHeaderLocaleResolver...so we don't need to fetch header in controller method
+	 */
 	
 	@Bean
-	public ResourceBundleMessageSource messageSource() {
-		ResourceBundleMessageSource source = new ResourceBundleMessageSource();
-		source.setBasename("message");
-		return source;
+	public LocaleResolver localeResolver()
+	{
+		AcceptHeaderLocaleResolver localeResolver = new AcceptHeaderLocaleResolver();
+		localeResolver.setDefaultLocale(Locale.US);
+		return localeResolver;
 	}
+	
+	
+	/*
+	 * Alternative is to configure it in application.properties
+	 * spring.messages.basename = message
+	 * 
+	 * @Bean public ResourceBundleMessageSource messageSource() {
+	 * ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+	 * source.setBasename("message"); return source; }
+	 */
+	/*
+	 * @Bean public ResourceBundleMessageSource messageSource() {
+	 * ResourceBundleMessageSource source = new ResourceBundleMessageSource();
+	 * source.setBasename("message"); return source; }
+	 */
 
 }
